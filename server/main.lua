39f759e9e2ba1e5c7cd84f1ebabea0f2a09bf839
QuantumMalice <acrandall2016@gmail.com>
2022-07-26 22:02:58 -0600

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,4 +1,4 @@
-QBCore.Commands.Add("vehdebug", "Vehicle debugger for tune balancing", {}, false, function(source)
+QBCore.Commands.Add(Config.Command, "Vehicle debugger for tune balancing", {}, false, function(source)
     local src = source
     TriggerClientEvent("vDebug:toggle", src)
 end, Config.Permission)
